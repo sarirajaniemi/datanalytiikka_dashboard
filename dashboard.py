797968a026c7c39624fa883a6a113215ecf8e511
Sari <c3rasa03@students.oamk.fi>
2025-10-01 12:57:29 +0300

    Dashboard tehtävä valmis

--- dashboard.py
+++ dashboard.py
@@ -23,10 +23,38 @@ Sold
 
 st.write("Yhteenlaskettu myyntien määrä:", Sold.sum())
 
+Game= df["Game"]
+Game
+
+yhdistetty = pd.concat([Game,Sold],axis=1)
+yhdistetty
 
 
+df = pd.read_csv("ps4sales.csv")
+lista = df["Game"].unique()
+lista
+
+Sold= df[["Game", "Sold"]]
+Sold
 
 
+#fig =plt.figure()
+#plt.bar ["Game"], 
+#plt.xticks(rotation=90, fontsize=5)
+#st.pyplot(fig)
+df = df.dropna(subset=["Game", "Sold"])
+df["Sold"] = pd.to_numeric(df["Sold"], errors="coerce")
+
+# Halutessa top-20, jotta x-akseli ei tukkeudu
+top = df.sort_values("Sold", ascending=False).head(20)
+
+# 2a) Pystypylväät
+fig, ax = plt.subplots()
+ax.bar(top["Game"], top["Sold"])
+ax.set_xlabel("Game")
+ax.set_ylabel("Sold")
+plt.xticks(rotation=90, fontsize=6)
+st.pyplot(fig)
 
 
 
